7f4dd023d95f50bf57c9f42fa41cb2e72a5d3971
Bare <bareknuckle@nah.com>
2018-12-17 11:24:32 -0500

    day 1 part 2

--- one/src/main.rs
+++ one/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::Read;
 
@@ -7,9 +8,19 @@ fn main() {
     file.read_to_string(&mut input).unwrap();
 
     let mut freq = 0;
+    let mut seen: HashSet<i32> = HashSet::new();
+    seen.insert(freq);
 
-    for line in input.lines(){
-        freq = freq + line.parse::<i32>().unwrap();
+    'outer: loop {
+        for line in input.lines() {
+            freq = freq + line.parse::<i32>().unwrap();
+
+            if seen.contains(&freq) {
+                break 'outer;
+            } else {
+                seen.insert(freq);
+            }
+        }
     }
 
     println!("Freq: {}", freq);
