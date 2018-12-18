0d8bc38a1bb8bb1b6c20d10547e21e9026416aab
Bare <bareknuckle@nah.com>
2018-12-18 12:13:08 -0500

    day 2 - part 2

--- two/src/main.rs
+++ two/src/main.rs
@@ -6,6 +6,46 @@ fn main() {
     let mut input = String::new();
     file.read_to_string(&mut input).unwrap();
 
+    println!("{}", part_one(&mut input));
+
+    println!("{}", part_two(&mut input));
+}
+
+fn part_two(input: &mut String) -> String {
+    let mut lines = input.lines().collect::<Vec<_>>();
+    let mut first = "";
+
+    loop {
+        if lines.len() > 0 {
+            first = lines.remove(0);
+        } else {
+            panic!("It's all wrong, Pete Tong!!!");
+        }
+
+        for line in &lines {
+            let mut v = String::new();
+            let mut not_matches = 0;
+
+            for (chr_a, chr_b) in first.chars().zip(line.chars()) {
+                if chr_a == chr_b {
+                    v.push(chr_a);
+                } else {
+                    not_matches += 1;
+                }
+
+                if not_matches > 1 {
+                    break;
+                }
+            }
+
+            if not_matches == 1 {
+                return v;
+            }
+        }
+    }
+}
+
+fn part_one(input: &mut String) -> usize {
     let mut two = 0;
     let mut three = 0;
     for line in input.lines() {
@@ -28,7 +68,5 @@ fn main() {
         }
     }
 
-    let checksum: usize = two * three;
-
-    println!("Checksum: {}", checksum);
+    two * three
 }
